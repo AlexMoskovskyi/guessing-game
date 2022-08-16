17f08e1be323a10a0cb2d9a7dbe7d4882e3c4848
Aleksandr <mscw81@gmail.com>
2022-08-16 20:57:48 +0300

    feat: modified js file

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        return this.result = Math.round((this.min + this.max) / 2)
     }
 
     lower() {
-
+        this.max = this.result
     }
 
     greater() {
-
+        this.min = this.result
     }
 }
 
